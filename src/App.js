9c9f0bc7b7d7ab9891752f79c269874052a7d7bf
Zeynep Dağdeviren <av.zeynepdagd@gmail.com>
2025-01-04 15:18:57 +0300

    örnek kodlar yazıldı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <>
-    
-    App
-    
-    </>
-  );
+  const [paragrafGoster, paragrafIslem] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      paragrafGoster(false);
+    }, 2000);
+  });
+
+  return <>{paragrafGoster && <p id="p1">App</p>}</>;
 }
 
 export default App;
