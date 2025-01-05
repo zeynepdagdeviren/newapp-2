84eb872d144d0bcba975367c8a098bb96fd0328f
Zeynep Dağdeviren <av.zeynepdagd@gmail.com>
2025-01-05 16:02:41 +0300

    rehebr örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-
+import { useEffect, useState } from "react";
+import Kisi from "./kisi";
 
 function App() {
+  
+  const [veri, veriGuncelle] = useState([])
+  
+  useEffect(()=> {
+
+    const LSVeriCek = async () => localStorage.getItem("rehberJSON")
+
+    async function rehberCek() {
+      const rehberJSON = await LSVeriCek()
+      const rehberVeri = JSON.parse(rehberJSON)
+
+      veriGuncelle(rehberVeri)
+    }
+
+    rehberCek()
+
+  }, [])
+  
   return (
+
+    
     <>
-    
-    App
-    
+      <section className="container mt-5">
+        <h1>Rehber</h1>
+
+        <div className="row">
+          
+          {veri.map( eleman => <Kisi key={eleman.id} ad={eleman.ad} tel={eleman.tel} />)}
+          
+        </div>
+      </section>
     </>
   );
 }
---
+++ src/kisi.jsx
@@ -0,0 +1,12 @@
+function Kisi({ad,tel}) {
+  return (
+    <div className="col-lg-3">
+      <div className="alert alert-info">
+        <h2> {ad}</h2>
+        <p> {tel}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Kisi
