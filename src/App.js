23a115d1c28e00a2c7ff698cea915e28e51b11f1
Zeynep Dağdeviren <av.zeynepdagd@gmail.com>
2025-01-05 15:00:17 +0300

    örnek fetch kodlar yazıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+
+const [yukleniyor, yukleniyorGuncelle] = useState(true)
+
+const [data, setData] = useState([]);
+
+useEffect(()=>{
+  
+  async function fetchData() {
+   
+    
+      const response = await fetch('https://jsonplaceholder.org/users');
+      const jsObjectData = await response.json();
+      setData(jsObjectData);
+      yukleniyorGuncelle(false)
+  };
+
+    fetchData();
+
+
+
+
+  
+}, []);
+
   return (
     <>
-    
-    App
+    { yukleniyor && <p> Yükleniyor </p>}
+    { data.map( kisi => <p key={kisi.id}> {kisi.firstname}</p>)}
     
     </>
   );
